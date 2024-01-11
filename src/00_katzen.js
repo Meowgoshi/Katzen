abacce0fac91393bf3788bc01c0ba74d73083bc8
Tino <155625923+Meowgoshi@users.noreply.github.com>
2024-01-11 12:37:43 +0000

    00 / package updatet

--- src/00_katzen.js
+++ src/00_katzen.js
@@ -1,1 +1,11 @@
-console.log("Die Fellnasen heißen Yuffie, Legoshi und Ripley.");
+//console.log("Die Fellnasen heißen Yuffie, Legoshi und Ripley.");  //Ausgabe
+
+// let k1 = "Yuffie", k2 = "Legoshi", k3 = "Ripley";
+
+const prompt = require('prompt-sync')({sigint: true});
+
+let catName = prompt("Name: ");
+let catBirth = prompt("Geboren: ");
+let catJoin = prompt("Eingezogen: ");
+
+console.log(catName + " ist am " + catBirth + "geboren worden und am " + catJoin + " eingezogen.")
